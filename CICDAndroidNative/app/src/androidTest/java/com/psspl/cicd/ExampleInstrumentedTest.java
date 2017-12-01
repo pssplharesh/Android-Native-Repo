5402241618028d1786a6f767b5021ebde0d162ad
pssplharesh <Haresh@prakashinfotech.com>
2017-12-01 15:28:20 +0530

    Add CICD Code

--- CICDAndroidNative/app/src/androidTest/java/com/psspl/cicd/ExampleInstrumentedTest.java
+++ CICDAndroidNative/app/src/androidTest/java/com/psspl/cicd/ExampleInstrumentedTest.java
@@ -2,8 +2,15 @@ package com.psspl.cicd;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +23,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivity=new ActivityTestRule<MainActivity>(MainActivity.class);
     @Test
     public void useAppContext() throws Exception {
+
+
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.psspl.cicd", appContext.getPackageName());
+
+
+        Espresso.onView(ViewMatchers.withId(R.id.eteditbxo)).perform(ViewActions.clearText());
+        Espresso.onView(ViewMatchers.withId(R.id.eteditbxo)).perform(ViewActions.typeText("Test"));
+        Thread.sleep(2000);
+        Espresso.onView(ViewMatchers.withId(R.id.eteditbxo)).check(ViewAssertions.matches(ViewMatchers.withText("Test")));
+
     }
 }
--- CICDAndroidNative/app/src/main/java/com/psspl/cicd/MainActivity.java
+++ CICDAndroidNative/app/src/main/java/com/psspl/cicd/MainActivity.java
@@ -2,13 +2,15 @@ package com.psspl.cicd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText eteditbxo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eteditbxo=(EditText) findViewById(R.id.eteditbxo);
        // sdfsd
     }
 }
