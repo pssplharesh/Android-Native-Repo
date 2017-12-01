a0c57ab0f8cfd8b39f6b4f0db87bbe20955f69e9
pssplharesh <Haresh@prakashinfotech.com>
2017-12-01 19:19:59 +0530

    Add CICD Code

--- CICDAndroidNative/app/src/main/java/com/psspl/cicd/MainActivity.java
+++ CICDAndroidNative/app/src/main/java/com/psspl/cicd/MainActivity.java
@@ -12,7 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         eteditbxo=(EditText) findViewById(R.id.eteditbxo);
 
-        // This tested
-       // sdfsd
     }
 }
