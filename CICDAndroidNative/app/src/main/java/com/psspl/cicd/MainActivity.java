ca2b3c12f79f0db90310112f2d288c216a113f11
pssplharesh <Haresh@prakashinfotech.com>
2017-12-04 18:30:44 +0530

    test

--- CICDAndroidNative/app/src/main/java/com/psspl/cicd/MainActivity.java
+++ CICDAndroidNative/app/src/main/java/com/psspl/cicd/MainActivity.java
@@ -4,32 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.testfairy.TestFairy;
+
 public class MainActivity extends AppCompatActivity {
     private EditText eteditbxo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TestFairy.begin(this, "e052acbf9c9ed810d85a9bdb6e28c54d8f3e211c");
         eteditbxo=(EditText) findViewById(R.id.eteditbxo);
-        /*dkljdfgjdfg
-        * gdfg
-        * dfgdfg
-        * dfg
-        * dfg
-        * dfg
-        * dfgzxzxczxc
-        * zxczx
-        * czx
-        * c
-        * sdfsdf
-        * sdf
-        * sdfd
-        * fsdf
-        * ddsf
-        * sdf
-        *
-        * dfg
-        * */
+
+
 
     }
 }
