23892413f35c678fd72c75602ab2b962aa7ffd23
pssplharesh <Haresh@prakashinfotech.com>
2017-12-07 16:03:37 +0530

    mobile center integrate

--- CICDAndroidNative/app/src/main/java/com/psspl/cicd/MainActivity.java
+++ CICDAndroidNative/app/src/main/java/com/psspl/cicd/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 import com.testfairy.TestFairy;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TestFairy.begin(this, "6681215ff9cea86f7d600c77108bb413c8022890");
+        AppCenter.start(getApplication(), "ecf5bd41-08a7-41e1-b4e1-6d97f4e8ffe5",
+                Analytics.class, Crashes.class);
         eteditbxo=(EditText) findViewById(R.id.eteditbxo);
         // This is second version
         //this build update again
